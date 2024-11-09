313347e28e358c828559e8834bccdb0a20979bd0
William-Noble <william.noble1@ontariotechu.net>
2024-11-09 15:26:20 -0500

    updated mongodb URI to connect to database

--- config/db.js
+++ config/db.js
@@ -1,1 +1,1 @@
-module.exports = {"URI":"mongodb://127.0.0.1:27017/BookLib"}
+module.exports = {"URI":"mongodb+srv://proj1-websiteaccess:1cXZmsoMcHVHHHSv@cluster0.vwbjn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"}
