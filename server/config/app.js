d0379be4e6b920490371d8bf31d0ccfae668ea38
William-Noble <william.noble1@ontariotechu.net>
2024-11-26 13:27:38 -0500

    Added user mongoose schema and fixed several routing errors

--- server/config/app.js
+++ server/config/app.js
@@ -54,15 +54,15 @@ app.use(passport.session());
 app.use(flash());
 
 // view engine setup
-app.set('../views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, '../views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, '../public')));
-app.use(express.static(path.join(__dirname, '../node_modules')));
+app.use(express.static(path.join(__dirname, '../../public')));
+app.use(express.static(path.join(__dirname, '../../node_modules')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/incidents',incidentRouter);
