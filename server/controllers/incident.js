384c6fbe3c7a80cde66ea84b55352aec5714df00
William-Noble <william.noble1@ontariotechu.net>
2024-11-26 13:03:04 -0500

    seperated the route functions into the controllers file and require authentication function for performing database operations

---
+++ server/controllers/incident.js
@@ -0,0 +1,123 @@
+var express = require('express');
+var router = express.Router();
+let mongoose = require('mongoose');
+
+// telling my router that I have this model
+let Incident = require('../model/incident');
+
+// function for displaying the incidents list
+module.exports.displayIncidentslist = async (req,res,next)=>{
+    try{
+        const IncidentList = await incidentModel.find();
+        res.render('incident/incidentList',{
+            title:'Incidents',
+            IncidentList:IncidentList
+        })
+    }
+    catch(err){
+        console.error(err)
+        res.render('incident/incidentList',{
+            error:'Error on Server'})
+    }
+};
+
+// function for displaying the add page
+module.exports.displayAddPage = (req,res,next)=>{
+    try{
+        res.render('incident/add',{
+            title: 'Add New Incident'
+        })
+    }
+    catch(err)
+    {
+        console.error(err);
+        res.render('incident/incidentList',{
+            error:'Error on the server'
+        })
+    }
+};
+
+// function for processing the post operation for the add page
+module.exports.processAddPage = (req,res,next)=>{
+    try{
+        let newIncident = incidentModel({
+            "type":req.body.type,
+            "time":req.body.time,
+            "location":req.body.location,
+            "involvedParties":req.body.involvedParties,
+            "overview":req.body.overview,
+            "damages":req.body.damages,
+            "state":req.body.state
+        });
+        incidentModel.create(newIncident).then(()=>{
+            res.redirect('/incidents');
+        })
+    }
+    catch(err)
+    {
+        console.error(err);
+        res.render('/incidents',{
+            error:'Error on the server'
+        })
+    }
+};
+
+// function for displaying the edit page
+module.exports.displayEditPage = async (req,res,next)=>{
+    try{
+        const id = req.params.id;
+        const incidentToEdit= await incidentModel.findById(id);
+        res.render('incident/edit',
+            {
+                title:'Edit Incident',
+                incidentModel:incidentToEdit
+            }
+        )
+    }
+    catch(err)
+    {
+        console.error(err);
+        next(err); // passing the error
+    }
+};
+
+// function for processing the post operation on the edit page
+module.exports.processEditPage = async (req,res,next)=>{
+    try{
+        let id=req.params.id;
+        let updatedIncident = {
+            "type":req.body.type,
+            "time":req.body.time,
+            "location":req.body.location,
+            "involvedParties":req.body.involvedParties,
+            "overview":req.body.overview,
+            "damages":req.body.damages,
+            "state":req.body.state
+        };
+        await incidentModel.findByIdAndUpdate(id,updatedIncident).then(()=>{
+            res.redirect('/incidents')
+        })
+    }
+    catch(err){
+        console.error(err);
+        res.render('/incidents',{
+            error:'Error on the server'
+        })
+    }
+};
+
+// function for performing the delete operation
+module.exports.performDelete = (req,res,next)=>{
+    try{
+        let id=req.params.id;
+        incidentModel.deleteOne({_id:id}).then(()=>{
+            res.redirect('/incidents')
+        })
+    }
+    catch(err){
+        console.error(err);
+        res.render('/incidents',{
+            error:'Error on the server'
+        })
+    }
+};
--- server/routes/incident.js
+++ server/routes/incident.js
@@ -2,111 +2,33 @@ var express = require('express');
 var router = express.Router();
 let mongoose = require('mongoose');
 let incidentModel = require('../model/incident')
-router.get('/',async(req,res,next)=>{
-    try{
-        const IncidentList = await incidentModel.find();
-        res.render('incident/incidentList',{
-            title:'Incidents',
-            IncidentList:IncidentList
-        })
-    }
-    catch(err){
-        console.error(err)
-        res.render('incident/incidentList',{
-            error:'Error on Server'})
-    }
-})
-router.get('/add',async(req,res,next)=>{
-    try{
-        res.render('incident/add',{
-            title: 'Add New Incident'
-        })
-    }
-    catch(err)
-    {
-        console.error(err);
-        res.render('incident/incidentList',{
-            error:'Error on the server'
-        })
-    }
-});
-router.post('/add',async(req,res,next)=>{
-    try{
-        let newIncident = incidentModel({
-            "type":req.body.type,
-            "time":req.body.time,
-            "location":req.body.location,
-            "involvedParties":req.body.involvedParties,
-            "overview":req.body.overview,
-            "damages":req.body.damages,
-            "state":req.body.state
-        });
-        incidentModel.create(newIncident).then(()=>{
-            res.redirect('/incidents');
-        })
-    }
-    catch(err)
-    {
-        console.error(err);
-        res.render('/incidents',{
-            error:'Error on the server'
-        })
-    }
-});
+let incidentController = require('../controllers/incident.js')
 
-router.get('/edit/:id',async(req,res,next)=>{
-    try{
-        const id = req.params.id;
-        const incidentToEdit= await incidentModel.findById(id);
-        res.render('incident/edit',
-            {
-                title:'Edit Incident',
-                incidentModel:incidentToEdit
-            }
-        )
-    }
-    catch(err)
-    {
-        console.error(err);
-        next(err); // passing the error
-    }
-});
-router.post('/edit/:id',async(req,res,next)=>{
-    try{
-        let id=req.params.id;
-        let updatedIncident = {
-            "type":req.body.type,
-            "time":req.body.time,
-            "location":req.body.location,
-            "involvedParties":req.body.involvedParties,
-            "overview":req.body.overview,
-            "damages":req.body.damages,
-            "state":req.body.state
-        };
-        await incidentModel.findByIdAndUpdate(id,updatedIncident).then(()=>{
-            res.redirect('/incidents')
-        })
-    }
-    catch(err){
-        console.error(err);
-        res.render('/incidents',{
-            error:'Error on the server'
-        })
-    }
-});
+// checks if the user is logged in and if not sends them to the log in page
+function requireAuth(req,res,next)
+{
+	if(!req.isAuthenticated())
+	{
+		return res.redirect('/login');
+	}
+	next();
+}
 
-router.get('/delete/:id',async(req,res,next)=>{
-    try{
-        let id=req.params.id;
-        incidentModel.deleteOne({_id:id}).then(()=>{
-            res.redirect('/incidents')
-        })
-    }
-    catch(err){
-        console.error(err);
-        res.render('/incidents',{
-            error:'Error on the server'
-        })
-    }
-});
+/* Read Operation --> Get route for displaying the incidents list */
+router.get('/',incidentController.displayIncidentslist)
+
+/* Create Operation --> Get route for displaying the Add Page */
+router.get('/add',requireAuth,incidentController.displayAddPage);
+/* Create Operation --> Post route for processing the Add Page */
+router.post('/add',requireAuth,incidentController.processAddPage);
+
+/* Update Operation --> Get route for displaying the Edit Page */
+router.get('/edit/:id',requireAuth,incidentController.displayEditPage);
+/* Update Operation --> Post route for processing the Edit Page */ 
+router.post('/edit/:id',requireAuth,incidentController.processEditPage);
+
+/* Delete Operation --> Get route to perform Delete Operation */
+router.get('/delete/:id',requireAuth,incidentController.performDelete);
+
+//export :)
 module.exports = router;
